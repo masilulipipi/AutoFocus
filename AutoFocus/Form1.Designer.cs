cb16d62d52b4dc3abe65dff1e83454320a21ac2c
masilulipipi <masilulipipi@gmail.com>
2019-11-28 10:06:58 -0300

    autofocus camara sony

---
+++ AutoFocus/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AutoFocus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtip1 = new System.Windows.Forms.TextBox();
+            this.txtip2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "AutoFocus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "172.21.";
+            // 
+            // txtip1
+            // 
+            this.txtip1.Location = new System.Drawing.Point(75, 29);
+            this.txtip1.Name = "txtip1";
+            this.txtip1.Size = new System.Drawing.Size(42, 20);
+            this.txtip1.TabIndex = 0;
+            this.txtip1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtip_KeyPress);
+            // 
+            // txtip2
+            // 
+            this.txtip2.Location = new System.Drawing.Point(139, 29);
+            this.txtip2.Name = "txtip2";
+            this.txtip2.Size = new System.Drawing.Size(42, 20);
+            this.txtip2.TabIndex = 1;
+            this.txtip2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ".";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reseteo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 101);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtip2);
+            this.Controls.Add(this.txtip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Autofoco";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtip1;
+        private System.Windows.Forms.TextBox txtip2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ AutoFocus/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+
+namespace AutoFocus
+{
+    
+    
+    public partial class Form1 : Form
+    {
+        private string numeroip1,numeroip2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numeroip1 = txtip1.Text;
+            numeroip2 = txtip2.Text;
+                      
+            System.Diagnostics.Process.Start("http://admin:admin@172.21."+numeroip1+"."+numeroip2+"/command/focuszoom.cgi?FzMove=focus,auto,2");
+           
+
+        }
+
+        private void txtip_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)(Keys.Tab))
+            {
+                e.Handled = true;
+                SendKeys.Send("{TAB}");
+            }
+
+            if (e.KeyChar == (char)(Keys.Enter))
+            {
+                e.Handled = true;
+                SendKeys.Send("{TAB}");
+            }
+
+            else if (Char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = true;
+                SendKeys.Send("{TAB}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numeroip1 = txtip1.Text;
+            numeroip2 = txtip2.Text;
+
+            System.Diagnostics.Process.Start("http://admin:admin@172.21." + numeroip1 + "." + numeroip2 + "/command/main.cgi?system=reboot");
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)(Keys.Enter))
+            {
+                numeroip1 = txtip1.Text;
+                numeroip2 = txtip2.Text;
+
+                System.Diagnostics.Process.Start("http://admin:admin@172.21." + numeroip1 + "." + numeroip2 + "/command/focuszoom.cgi?FzMove=focus,auto,2");
+            }
+
+            
+        }
+    }
+}
